18b6c9feeac90f96f943017cafb78af76526ede1
German <amaliyabag@gmail.com>
2024-12-08 12:08:16 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*"
+  })
   app.use(json({ limit: "20mb" }));
   app.use(cookieParser());
   app.useGlobalPipes(
